2361df669b26b5089035df466d17a3be13f3136b
Yoseph Endale <endaleyoseph1@gmail.com>
2023-02-11 09:46:00 -0800

    fix2

--- models/base_model.py
+++ models/base_model.py
@@ -6,8 +6,8 @@ with public instance attribute
 """
 
 
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 import models
 
 
@@ -32,19 +32,19 @@ class BaseModel:
         if kwargs:
             for k, v in kwargs.items():
                 if "created_at" == k:
-                    self.created_at = datetime.datetime. \
+                    self.created_at = datetime. \
                                       strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 elif "updated_at" == k:
-                    self.updated_at = datetime.datetime. \
+                    self.updated_at = datetime. \
                                       strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 elif "__class__" == k:
                     pass
                 else:
                     setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.today()
-            self.updated_at = datetime.datetime.today()
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
@@ -58,7 +58,7 @@ class BaseModel:
 
     def save(self):
         """Updates the current time"""
-        self.updated_at = datetime.datetime.today()
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
@@ -66,7 +66,7 @@ class BaseModel:
         dic = {}
         dic["__class__"] = self.__class__.__name__
         for k, v in self.__dict__.items():
-            if isinstance(v, (datetime.datetime, )):
+            if isinstance(v, (datetime, )):
                 dic[k] = v.isoformat()
             else:
                 dic[k] = v
